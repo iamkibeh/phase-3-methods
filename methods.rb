13a7ffe5e19cd555eb31a9e89248ec44af974c3d
iamkibeh <kibet.immanuel@student.moringaschool.com>
2022-10-17 22:16:45 +0300

    complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+ def greet name
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name="Hello, programmer!")
+    puts "Hello, #{name}!"
+end
+
+def add(a,b)
+    a+b
+end
+
+def halve(num)
+    num.class == Integer ? num/2 : nil
+end
+
+# puts greet("Naureen")
